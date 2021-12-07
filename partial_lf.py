a5c12f3057680b4eb38224a466ba508036454961
cylammarco <cylammarco@gmail.com>
2021-12-07 14:12:35 +0200

    Included high precision WDLFs with 0.1Gyr burst SFH.

--- partial_lf.py
+++ partial_lf.py
@@ -6,15 +6,16 @@ wdlf = theoretical_lf.WDLF()
 wdlf.set_ifmr_model('C08')
 wdlf.compute_cooling_age_interpolator()
 
-Mag = np.arange(0., 20., 0.01)
-age_list = 1E9 * np.arange(8.8, 15., 0.1)
+Mag = np.arange(0., 20., 0.25)
+age_list = 1E9 * np.arange(0.1, 15., 0.1)
 
 for age in age_list:
     wdlf.set_sfr_model(mode='burst', age=age, duration=1e8)
     wdlf.compute_density(Mag=Mag,
                          normed=False,
+                         epsabs=1e-12,
+                         epsrel=1e-11,
+                         n_points=1000,
                          save_csv=True)
-
     wdlf.plot_wdlf(display=False,
-                   savefig=True,
-                   filename='{}Gyr_burst.png'.format(age))
+                   savefig=True)
--- sfh_mcmc.py
+++ sfh_mcmc.py
@@ -1,11 +1,9 @@
 import emcee
-import corner
 import numpy as np
-from scipy import interpolate as itp
 from matplotlib.pyplot import *
 from WDLFBuilder import theoretical_lf
-ion()
 
+ion()
 
 
 def log_prior(theta):
@@ -16,55 +14,71 @@ def log_prior(theta):
 
 
 def log_probability(rel_norm, obs, err, model_list):
+    rel_norm /= np.sum(rel_norm)
     if not np.isfinite(log_prior(rel_norm)):
         return -np.inf
     model = np.nansum(rel_norm[:, None] * model_list, axis=0)
-    # Assume Poisson noise in both model and observed WDLFs
-    err[err<=0.] = 1e-10
-    log_likelihood = -np.nansum(((model-obs)/err)**2.)
-    if np.isfinite(log_likelihood):
-        return log_likelihood
-    else:
+    # penalise non-detection
+    err[obs <= 0.] = np.nanmin(err)
+    # Conserve the number density
+    '''
+    model = model * np.nansum(obs) / np.nansum(model)
+    if (model < 0).any() or (~np.isfinite(model)).any():
         return -np.inf
+    '''
+    # mask out parts without observation
+    log_likelihood = -np.nansum(((model - obs) / err)**2.)
+    return log_likelihood
 
 
 # Collect all the partial WDLFs, in chunks of 0.5 Gyr
 partial_wdlf = []
-age = np.arange(2.0, 10.0, 0.5)
+age = np.arange(0.5, 10.0, 0.5)
 for i in age:
-    filename1 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(i)
-    filename2 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(i+0.1)
-    filename3 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(i+0.2)
-    filename4 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(i+0.3)
-    filename5 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(i+0.4)
-    n_temp = np.loadtxt(filename1, delimiter=',')[:,1]
-    n_temp += np.loadtxt(filename2, delimiter=',')[:,1]
-    n_temp += np.loadtxt(filename3, delimiter=',')[:,1]
-    n_temp += np.loadtxt(filename4, delimiter=',')[:,1]
-    n_temp += np.loadtxt(filename5, delimiter=',')[:,1]
-    #n_temp /= np.max(n_temp)
+    filename1 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(
+        i)
+    filename2 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(
+        i + 0.1)
+    filename3 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(
+        i + 0.2)
+    filename4 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(
+        i + 0.3)
+    filename5 = '{0:.2f}Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv'.format(
+        i + 0.4)
+    n_temp = np.loadtxt(filename1, delimiter=',')[:, 1]
+    n_temp += np.loadtxt(filename2, delimiter=',')[:, 1]
+    n_temp += np.loadtxt(filename3, delimiter=',')[:, 1]
+    n_temp += np.loadtxt(filename4, delimiter=',')[:, 1]
+    n_temp += np.loadtxt(filename5, delimiter=',')[:, 1]
+    n_temp /= np.nansum(n_temp)
     partial_wdlf.append(n_temp.copy())
     n_temp = None
 
-
 # Get the magnitude
-mag = np.loadtxt('0.10Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv', delimiter=',')[:,0]
+mag = np.loadtxt(
+    '0.10Gyr_burst_C16_C08_montreal_co_da_20_montreal_co_da_20_montreal_co_da_20.csv',
+    delimiter=',')[:, 0]
 
 # Generate a WDLF with 1 Gyr burst at 2-3 Gyr and 5-6 Gyr
-obs_wdlf = partial_wdlf[2] + partial_wdlf[3] + partial_wdlf[6] + partial_wdlf[7]
+obs_wdlf = partial_wdlf[3] + partial_wdlf[4] + partial_wdlf[9] + partial_wdlf[
+    10]
+obs_wdlf /= np.nansum(obs_wdlf)
 
-obs_wdlf_err = np.sqrt(obs_wdlf)
-obs_wdlf_err[obs_wdlf<=0.] = np.sqrt(min(obs_wdlf[obs_wdlf>0.]))
+# assume 2% error (S/N of 50) and a 1% noise floor
+obs_wdlf_err = obs_wdlf * 0.02 + np.percentile(obs_wdlf[obs_wdlf > 0], 1.0)
 
-nwalkers = 100
+nwalkers = 500
 ndim = len(partial_wdlf)
 
 rel_norm = np.random.rand(nwalkers, ndim)
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(obs_wdlf, obs_wdlf_err, partial_wdlf))
-sampler.run_mcmc(rel_norm, 100000, progress=True)
+sampler = emcee.EnsembleSampler(nwalkers,
+                                ndim,
+                                log_probability,
+                                args=(obs_wdlf, obs_wdlf_err, partial_wdlf))
+sampler.run_mcmc(rel_norm, 10000, progress=True)
 
-flat_samples = sampler.get_chain(discard=10000, thin=15, flat=True)
+flat_samples = sampler.get_chain(discard=1000, thin=5, flat=True)
 
 #fig = corner.corner(flat_samples)
 
@@ -73,101 +87,156 @@ for i in range(ndim):
     solution[i] = np.percentile(flat_samples[:, i], [50.])
 
 real_sfh = np.zeros_like(age)
-real_sfh[2] = 1.
 real_sfh[3] = 1.
-real_sfh[6] = 1.
-real_sfh[7] = 1.
+real_sfh[4] = 1.
+real_sfh[9] = 1.
+real_sfh[10] = 1.
+
+solution /= np.nansum(solution)
+real_sfh /= np.nansum(real_sfh)
 
 figure(1)
-scatter(age, solution, label='MCMC')
-scatter(age, real_sfh, label='input')
+clf()
+step(age, solution, where='post', label='MCMC')
+step(age, real_sfh, where='post', label='input')
 legend()
 grid()
-xlabel('Lookback time / Gyr') 
-ylabel('Relative Star Formation Rate') 
+xlabel('Lookback time / Gyr')
+ylabel('Relative Star Formation Rate')
 tight_layout()
 savefig('two_bursts_sfh.png')
 
-sfh_model = itp.interp1d(age, solution)
-wdlf = theoretical_lf.WDLF()
-wdlf.set_ifmr_model('C08')
-wdlf.compute_cooling_age_interpolator()
-
-wdlf.set_sfr_model(mode='manual', age=max(age), sfr_model=sfh_model)
-_, recomputed_wdlf = wdlf.compute_density(Mag=mag,
-                         normed=True,
-                         save_csv=False)
+recomputed_wdlf = np.nansum(solution * np.array(partial_wdlf).T, axis=1)
 
 figure(2)
+clf()
 plot(mag, np.log10(obs_wdlf), label='Input WDLF')
-plot(mag, np.log10(recomputed_wdlf), label='Extracted WDLF')
-xlabel(r'M$_bol$ / mag')
+plot(mag, np.log10(recomputed_wdlf), label='Reconstructed WDLF')
+xlabel(r'M${_\mathrm{bol}}$ / mag')
 ylabel('log(arbitrary number density)')
-xlim(7.5, 20)
-ylim(-5, 0)
+xlim(0, 20)
+ylim(-7.5, -1)
 legend()
 grid()
 tight_layout()
 savefig('two_bursts_wdlf.png')
 
-
 # Decay SFH profile
-nwalkers = 100
-ndim = len(partial_wdlf)
-
-rel_norm = np.random.rand(nwalkers, ndim)
-
 wdlf = theoretical_lf.WDLF()
-
 wdlf.compute_cooling_age_interpolator()
-wdlf.set_sfr_model(mode='decay', age=6E9) 
+wdlf.set_sfr_model(mode='decay', age=6E9)
 
 _, obs_wdlf2 = wdlf.compute_density(Mag=mag, n_points=10)
-obs_wdlf_err2 = np.sqrt(obs_wdlf2)
-obs_wdlf_err2[obs_wdlf2<=0.] = np.sqrt(min(obs_wdlf2[obs_wdlf2>0.]))
+# assume 2% error (S/N of 50) and a 1% noise floor
+obs_wdlf_err2 = obs_wdlf2 * 0.02 + np.percentile(obs_wdlf2[obs_wdlf2 > 0], 1.0)
 
-sampler2 = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(obs_wdlf2, obs_wdlf_err2, partial_wdlf))
-sampler2.run_mcmc(rel_norm, 100000, progress=True)
+nwalkers = 500
+ndim = len(partial_wdlf)
 
-flat_samples2 = sampler2.get_chain(discard=10000, thin=15, flat=True)
+rel_norm2 = np.random.rand(nwalkers, ndim)
+
+sampler2 = emcee.EnsembleSampler(nwalkers,
+                                ndim,
+                                log_probability,
+                                args=(obs_wdlf2, obs_wdlf_err2, partial_wdlf))
+sampler2.run_mcmc(rel_norm2, 50000, progress=True)
+
+flat_samples2 = sampler2.get_chain(discard=5000, thin=5, flat=True)
 
 solution2 = np.zeros(ndim)
 for i in range(ndim):
     solution2[i] = np.percentile(flat_samples2[:, i], [50.])
 
 
+age2 = np.linspace(0, 10e9, 1000) / 1e9
+real_sfh2 = wdlf.sfr(np.linspace(0, 10e9, 1000))
+real_sfh2 /= (np.nansum(real_sfh2) * np.diff(age2)[0])
+solution2 /= np.nansum(solution2)
+
 figure(3)
-scatter(age, solution2/max(solution2), label='MCMC')
-plot(np.linspace(0, 10e9, 1000)/1e9, wdlf.sfr(np.linspace(0, 10e9, 1000)), label='input')
+clf()
+step(age, solution2 / 0.5, where='post', label='MCMC')
+plot(age2, real_sfh2, label='input')
 xlim(0, 10.)
 legend()
 grid()
-xlabel('Lookback time / Gyr') 
-ylabel('Relative Star Formation Rate') 
+xlabel('Lookback time / Gyr')
+ylabel('Relative Star Formation Rate')
 tight_layout()
 savefig('decay_sfh.png')
 
-
-sfh_model2 = itp.interp1d(age*1E9, solution2)
-wdlf2 = theoretical_lf.WDLF()
-wdlf2.set_ifmr_model('C08')
-wdlf2.compute_cooling_age_interpolator()
-
-wdlf2.set_sfr_model(mode='manual', age=6E9, sfr_model=sfh_model2)
-_, recomputed_wdlf2 = wdlf.compute_density(Mag=mag,
-                         normed=True)
+recomputed_wdlf2 = np.nansum(solution2 * np.array(partial_wdlf).T, axis=1)
 
 figure(4)
+clf()
 plot(mag, np.log10(obs_wdlf2), label='Input WDLF')
-plot(mag, np.log10(recomputed_wdlf2), label='Extracted WDLF')
+plot(mag, np.log10(recomputed_wdlf2), label='Reconstructed WDLF')
 xlabel(r'M$_bol$ / mag')
 ylabel('log(arbitrary number density)')
-xlim(7.5, 20)
-ylim(-5, 0)
+xlim(0, 20)
+ylim(-7, 0)
 legend()
 grid()
 tight_layout()
 savefig('decay_sfh_wdlf.png')
+
+
+# Decay and a burst between 2 and 3 Gyr
+obs_wdlf3 = obs_wdlf2 + partial_wdlf[3] + partial_wdlf[4]
+obs_wdlf_err3 = obs_wdlf3 * 0.02 + np.percentile(obs_wdlf3[obs_wdlf3 > 0], 1.0)
+
+nwalkers = 200
+ndim = len(partial_wdlf)
+
+rel_norm3 = np.random.rand(nwalkers, ndim)
+
+sampler3 = emcee.EnsembleSampler(nwalkers,
+                                ndim,
+                                log_probability,
+                                args=(obs_wdlf3, obs_wdlf_err3, partial_wdlf))
+sampler3.run_mcmc(rel_norm3, 200000, progress=True)
+
+flat_samples3 = sampler3.get_chain(discard=20000, flat=True)
+
+solution3 = np.zeros(ndim)
+for i in range(ndim):
+    solution3[i] = np.percentile(flat_samples3[:, i], [50.])
+
+
+age3 = np.linspace(0, 10e9, 1000) / 1e9
+real_sfh3 = wdlf.sfr(np.linspace(0, 10e9, 1000))
+real_sfh3[np.argmax(age3>2):np.argmin(age3<3)] += max(real_sfh3)
+real_sfh3 /= (np.nansum(real_sfh3) * np.diff(age3)[0])
+solution3 /= np.nansum(solution3)
+
+figure(5)
+clf()
+step(age, solution3, where='post', label='MCMC')
+plot(age3, real_sfh3, label='input')
+xlim(0, 10.)
+legend()
+grid()
+xlabel('Lookback time / Gyr')
+ylabel('Relative Star Formation Rate')
+tight_layout()
+savefig('decay_plus_burst_sfh.png')
+
+recomputed_wdlf3 = np.nansum(solution3 * np.array(partial_wdlf).T, axis=1)
+
+figure(6)
+clf()
+plot(mag, np.log10(obs_wdlf3), label='Input WDLF')
+plot(mag, np.log10(recomputed_wdlf3), label='Reconstructed WDLF')
+xlabel(r'M$_bol$ / mag')
+ylabel('log(arbitrary number density)')
+xlim(0, 20)
+ylim(-7, 0)
+legend()
+grid()
+tight_layout()
+savefig('decay_plus_burst_sfh_wdlf.png')
+
+
 
 
 
