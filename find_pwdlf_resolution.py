33ebc95b0df86f6ddbdbce64bd81824e14eeb7d3
cylammarco <cylammarco@gmail.com>
2023-02-12 23:59:17 +0200

    scripted up to find the set of pwdlfs that meet the minimum resolution requirement

--- find_pwdlf_resolution.py
+++ find_pwdlf_resolution.py
@@ -7,7 +7,7 @@ from scipy import interpolate
 
 
 data = []
-age_list_1 = np.arange(0.001, 0.100, 0.001)
+age_list_1 = np.arange(0.049, 0.100, 0.001)
 age_list_2 = np.arange(0.100, 0.350, 0.005)
 age_list_3 = np.arange(0.35, 15.01, 0.01)
 age_list_3dp = np.concatenate((age_list_1, age_list_2))
@@ -46,7 +46,7 @@ for i, d in enumerate(data):
 
 
 mag_resolution_itp = interpolate.UnivariateSpline(
-    age, mag_at_peak_density, s=len(age) / 100
+    age, mag_at_peak_density, s=len(age) / 150, k=5
 )
 
 
@@ -79,7 +79,10 @@ plt.savefig("age_peak_density_resolution.png")
 
 bin_02 = []
 bin_02_idx = []
+# to group the constituent pwdlfs into the optimal set of pwdlfs
+bin_02_pwdlf = np.zeros_like(age[1:]).astype('int')
 j = 0
+bin_number = 0
 stop = False
 for i, a in enumerate(age):
     if i < j:
@@ -93,25 +96,26 @@ for i, a in enumerate(age):
         print(j, end + tmp - start)
         if end + tmp - start < 0.0849257:
             end = end + tmp
+            bin_02_pwdlf[j] = bin_number
             j += 1
             if j >= len(age) - 1:
                 carry_on = False
                 stop = True
         else:
             carry_on = False
+            bin_number += 1
     if stop:
         break
     bin_02.append(end)
     bin_02_idx.append(i)
 
-print(bin_02)
-print(bin_02_idx)
-print(age[bin_02_idx])
-
 
 bin_05 = []
 bin_05_idx = []
+# to group the constituent pwdlfs into the optimal set of pwdlfs
+bin_05_pwdlf = np.zeros_like(age[1:]).astype('int')
 j = 0
+bin_number = 0
 stop = False
 for i, a in enumerate(age):
     if i < j:
@@ -124,28 +128,32 @@ for i, a in enumerate(age):
         tmp = mag_resolution_itp(age[j+1]) - mag_resolution_itp(age[j])
         if end + tmp - start < 0.2123142:
             end = end + tmp
+            bin_05_pwdlf[j] = bin_number
             j += 1
             if j >= len(age) - 1:
                 carry_on = False
                 stop = True
         else:
             carry_on = False
+            bin_number += 1
     if stop:
         break
     bin_05.append(end)
     bin_05_idx.append(i)
 
-print(bin_05)
-print(bin_05_idx)
-print(age[bin_05_idx])
 
 resolution_02 = np.array(bin_02[1:]) - np.array(bin_02[:-1])
 resolution_05 = np.array(bin_05[1:]) - np.array(bin_05[:-1])
 
 plt.figure(3)
-plt.scatter(bin_02[:-1], resolution_02, label='0.2 mag bin', s=5)
-plt.scatter(bin_05[:-1], resolution_05, label='0.5 mag bin', s=5)
+plt.scatter(bin_02[:-1], resolution_02, label='0.0849257 mag bin', s=5)
+plt.scatter(bin_05[:-1], resolution_05, label='0.2123142 mag bin', s=5)
+plt.legend()
 plt.ylabel('magnitude resolution')
 plt.xlabel('magnitude')
 plt.savefig('magnitude_resoltuion.png')
 
+
+# save the pdwdlf mapping
+np.save('pwdlf_bin_02_mapping.npy', bin_02_pwdlf)
+np.save('pwdlf_bin_05_mapping.npy', bin_05_pwdlf)
