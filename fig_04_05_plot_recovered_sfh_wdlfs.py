fixed the normalisation between two files

fig_04_05_plot_recovered_sfh_wdlfs.py
@@ -198,7 +198,7 @@ solution_upper = np.append(solution_upper, 0.0)
 solution_lower = np.append(solution_lower, 0.0)
 
 
-imf_normalisation = quad(imf_func, 0.9, 8.0)[0]
+imf_normalisation = quad(imf_func, 0.6, 8.0)[0]
 normalisation_this_work = (
     np.sum(obs_wdlf_optimal * resolution_optimal) / np.sum(recomputed_wdlf_optimal_lsq) / imf_normalisation
 )

fig_10_plot_compare_sfh.py
@@ -339,7 +339,7 @@ mag_bin_norm_this_work = np.concatenate(
 )
 
 
-imf_normalisation = quad(imf_func, 0.6, 7.0)[0]
+imf_normalisation = quad(imf_func, 0.6, 8.0)[0]
 normalisation_this_work = (
     np.sum(obs_wdlf_optimal * resolution_optimal) / np.sum(recomputed_wdlf_optimal_lsq) / imf_normalisation
 )